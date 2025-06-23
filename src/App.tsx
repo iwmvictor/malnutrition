7c944af6a7df8efb1747f39351ca11270fbd66a1
gilberthappi <gdushimimana6@gmail.com>
2025-06-23 20:34:27 +0200

    Add context

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { Toaster } from "react-hot-toast";
 import Providers from "./utils/Providers/Providers";
+import { AppProvider } from "./context/AppContext";
 
 const App = () => (
-  <>
+  <AppProvider>
     <Providers />
     <Toaster />
-  </>
+  </AppProvider>
 );
 
 export default App;
